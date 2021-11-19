c4b3afac19a6494c89ecab9259ab769808f934f8
Brendan Wu <wubrendan7@gmail.com>
2021-11-19 01:53:25 -0500

    Revert "Simple blog post routes"
    This reverts commit 983a4752599487fb85f268820dfb7dd2c989b271.

--- src/models/Post.ts
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({ title: 'string', description: 'string', date: 'string', author: 'string', category: 'string', markdownString: 'string' });
-const PostModal = mongoose.model('Post', schema);
-
-export default PostModal
